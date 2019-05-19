f628a158c431987d4825851f7237392ff97c8473
Eincherjar <marcinbarszcz123@gmail.com>
2019-05-19 17:28:55 +0200

    change animation time

--- src/components/hero.js
+++ src/components/hero.js
@@ -16,7 +16,7 @@ const PosedH2 = posed.h2({
   enter: {
     opacity: 1,
     y: 0,
-    delay: 3500,
+    delay: 2500,
   },
 })
 
@@ -52,7 +52,7 @@ const StyledSubtitle = styled(PosedH2)`
 
 const Hero = () => (
   <StyledHero>
-    <Typing speed={5}>
+    <Typing speed={20}>
       <Typing.Delay ms={500} />
       <StyledTitle>
         Witaj, <br /> jestem Marcin
